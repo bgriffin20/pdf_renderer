4e7126b3f6e4231bb0cde1c891eaa73fc6b93dab
Brad Griffin <bgriffin20@gmail.com>
2012-07-30 22:54:01 -0500

    Complete pdf renderer

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '54a44e200f74fd4ef0f5934419603d0d61044eba2cffa74f1665137b5e1a8feb85fc86381f473ba7caa4e1920d41fc883be26b02da24efa4d98a4de5deaa1593'
